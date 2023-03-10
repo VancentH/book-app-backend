cbad088161b00e05252c5ae018480567c8edafdd
hyde31456 <hyde31456@gmail.com>
2023-03-10 10:06:13 +0800

    <feat>: add global CORS configuration

--- src/main/java/com/example/bookappbackend/BookAppBackendApplication.java
+++ src/main/java/com/example/bookappbackend/BookAppBackendApplication.java
@@ -3,12 +3,27 @@ package com.example.bookappbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class BookAppBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookAppBackendApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**").allowedOrigins("http://localhost:80")
+						.allowedMethods("GET", "POST", "PUT", "DELETE").allowCredentials(true)
+						.maxAge(3600);
+			}
+		};
 	}
 
 }
