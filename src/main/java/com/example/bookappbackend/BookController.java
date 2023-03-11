ccad3c86cabf2cf179cd7170ea3e3c0daa3eda1d
hyde31456 <hyde31456@gmail.com>
2023-03-11 14:08:01 +0800

    <feat>: update api path

--- src/main/java/com/example/bookappbackend/BookController.java
+++ src/main/java/com/example/bookappbackend/BookController.java
@@ -1,15 +1,13 @@
 package com.example.bookappbackend;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api")
 public class BookController {
 
 	@GetMapping("/books")
